57262fa24ea865515140cf43d7ed42495c48526d
Adrián Garro <adriangarro@icloud.com>
2016-05-05 18:39:19 -0600

    Se añadió el algoritmo de búsqueda de caracteres

--- analyzer.asm
+++ analyzer.asm
@@ -1,24 +1,21 @@
-;; ******************************************
+;; **********************************************************************
 ;; File: analyzer.asm
 ;; Authors: Liza Chaves Carranza [2013]
 ;;          Marisol González Coto [2014]
 ;;          Elberth Adrian Garro Sanchez [2014088081]
-;; Utility: Analyze XML-HTML Code
+;; Utility: Analyze XML Code
 ;; Built with NASM Linux 64 bits
 ;; Copyright 2016 TEC
-;; ******************************************
-
-;; ******************************************
+;; **********************************************************************
 ;; Instructions for run this code:
 ;; Open terminal
 ;; Locate the code with cd, in my case:
 ;; cd '/media/psf/Home/Proyectos/NASM x64/Proyecto1_Arquitectura'
 ;; then write make
 ;; and finally write ./analyzer < file.extension
-;; ******************************************
-
+;; **********************************************************************
 ;;
-;; Include Macros Library
+;; include macros library
 ;;
 %include 'macros.mac'
 ;;
@@ -26,24 +23,32 @@
 ;;
 section .data
 	MAX_FILE_SZ equ 15 ; 4256
-	new_line: db 0xa
+	new_line: db 10
 		.len: equ $-new_line
+	dbg: db 'xD'
+		.len: equ $-dbg
 ;;
 ;; section containing non initialized data
 ;;
 section .bss
 	in_file resb MAX_FILE_SZ
-	test_file resb MAX_FILE_SZ
+	file_to_parse resb MAX_FILE_SZ
 ;;
 ;; section containing code
 ;;
 section .text
 	global _start
 _start:
-	read in_file, MAX_FILE_SZ
-	copy_buffer in_file, test_file
-	tolower test_file
-	write test_file, MAX_FILE_SZ
-	write new_line, new_line.len
-	;; 03/05/16 7:01 pm continue here...
+	input:
+		;; file read and process
+		read in_file, MAX_FILE_SZ
+		copy_buffer in_file, file_to_parse
+		to_lower file_to_parse
+	tag_test:
+		find_char file_to_parse, '<'
+	debug:
+		;; write file
+		write r9, MAX_FILE_SZ
+		;; write new line
+		write new_line, new_line.len
 	exit
