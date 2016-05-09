621e6089273a08739127c831b3a3c046109795e9
lizcha95 <lizchavca@gmail.com>
2016-05-09 00:16:34 -0600

    Se agregan funciones para indentar el archivo

--- xml_analyzer/xml_analyzer.asm
+++ xml_analyzer/xml_analyzer.asm
@@ -29,6 +29,10 @@ section .data
 		.len: equ $-error1_test1
 	error2_test1: db 'Error: falta > después de < en: x,y', 10
 		.len: equ $-error2_test1
+	test_errores: db 'Hay errores', 10
+		.len: equ $-test_errores
+	rows: db "Hay x filas", 10
+		.len: equ $-rows
 
 ;; **********************************************************************
 ;; section containing non initialized data
@@ -37,6 +41,7 @@ section .data
 section .bss
 	in_file resb MAX_FILE_SZ
 	file_to_parse resb MAX_FILE_SZ
+	file_to_ident resb 792
 
 ;; **********************************************************************
 ;; section containing code
@@ -45,12 +50,16 @@ section .bss
 section .text
 	global _start
 _start:
+
+	xor r11, r11
+
 	input_file:
 		read in_file, MAX_FILE_SZ
 		copy_buffer in_file, file_to_parse
 		to_lower file_to_parse
 	test1:
 		call individual_tag_test
+		call indentation
 	end_test:
 		exit
 
@@ -61,11 +70,15 @@ _start:
 ;;
 ;; individual_tag_test: check individual tags candidates in xml file
 ;;
+
 individual_tag_test:
 	;; buffer index
 	mov r8, -1
 	;; boolean check_tag_candidate
 	mov r9, 0
+	;; flag to see if there is any error in the file
+	mov r10, 0
+
 	.loop:
 		;; increment and compare
 		inc r8
@@ -74,6 +87,7 @@ individual_tag_test:
 			;; end test
 			ret
 		endif
+
 		;; go to search_tag or check_tag
 		cmp r9, 0
 		if e
@@ -89,9 +103,11 @@ individual_tag_test:
 			cmp byte [file_to_parse+r8], '>'
 			if e
 				write error1_test1, error1_test1.len
+				mov r10, 1
 			endif
 		endif
 		jmp .loop
+
 	.check_tag_candidate:
 		cmp byte [file_to_parse+r8], '>'
 		if e
@@ -101,10 +117,61 @@ individual_tag_test:
 			if e
 				write error2_test1, error2_test1.len
 				mov r9, 0
+				mov r10, 1
 			endif
 		endif
 		jmp .loop
 
 ;;
-;; Proc 2
+;; Procedure to correct indentation, creation of new file
 ;;
+
+indentation:
+
+	;; counter for file_to_parse buffer
+	xor r11, r11
+	;; counter for tags
+	xor r12, r12
+	;; counter for file_to_ident buffer
+	xor r13, r13
+
+	.comprobar:
+		cmp r10, 1
+			if e
+				write test_errores, test_errores.len
+			endif		
+		ret
+
+	.loop:
+
+	;; Revisa si el file_to_parse llegó al final
+	cmp byte[file_to_parse+r11], 0
+	;; Si llegó al final, imprime file_to_ident
+	if e
+		write file_to_indent, r13
+	;; Sino entonces sigue comparando todos los caracteres de file_to_parse
+	else
+		cmp byte[file_to_parse+r11], '<'
+		if e
+			;; Si es un tag, incrementa el contador de tags, y lo agrega a file_to_ident
+			inc r12
+			mov byte[file_to_indent+r13], [file_to_parse+r11]
+			
+			;; Ciclo para encontrar donde se cierra el tag
+			.find_end_tag:
+				inc r11
+				inc r13
+				cmp byte[file_to_parse+r11], '>'
+				;; Si encuentra el tag que cierra, lo agrega a file_to_ident y agrega un cambio de línea
+				if e
+					mov byte[file_to_indent+r13], [file_to_parse+r11]
+					inc r11
+					inc r13
+					mov byte[file_to_indent+r13], 10
+					inc r13
+					;; TODO: multiplicar r12 por 3, para hacer un ciclo que agregue los espacios en blanco
+
+
+
+	jmp .loop
+
