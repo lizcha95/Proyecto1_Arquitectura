b43fcbe020c4c8571b53bac38f0549bf9ef82687
Adrián Garro <adriangarro@icloud.com>
2016-05-09 13:48:06 -0600

    Revert "Cambios generales"
    This reverts commit bb160e94fe8aa1342b6a8932c3c2bcc1278b9f73.
    
    # Conflicts:
    #	xml_analyzer/xml_analyzer
    #	xml_analyzer/xml_analyzer.asm
    #	xml_analyzer/xml_analyzer.o

--- xml_analyzer/xml_analyzer.asm
+++ xml_analyzer/xml_analyzer.asm
@@ -1,7 +1,7 @@
 ;; **********************************************************************
 ;; File: xml_analyzer.asm
-;; Authors: Elberth Adrián Garro Sanchez [2014088081]
-;; Utility: Analyzes XML Code
+;; Authors: Elberth Adrian Garro Sanchez [2014088081]
+;; Utility: Analyze XML Code
 ;; Built with NASM Linux 64 bits
 ;; Copyright 2016 TEC
 ;;
@@ -16,41 +16,28 @@
 ;; **********************************************************************
 ;; include macros library
 ;; **********************************************************************
-
 %include 'macros.mac'
 
 ;; **********************************************************************
 ;; section containing initialized data
 ;; **********************************************************************
-
 section .data
-	;; numeric constants
 	MAX_FILE_SZ equ 792 ; 4256
-	;; two dots
-	DOTS db ':'
-	;; new line
-	NEW_LINE db 10
-	;; test1 strings
-	test1_init: db 'Ejecutando verificación de tags individuales en xml...', 10
-		.len: equ $-test1_init
-	error1_test1: db 'Error: falta < antes de > en '
+	error1_test1: db 'Error: falta < antes de > en línea: x,y', 10
 		.len: equ $-error1_test1
-	error2_test1: db 'Error: falta > después de < en '
+	error2_test1: db 'Error: falta > despues de < en línea: x,y', 10
 		.len: equ $-error2_test1
 
 ;; **********************************************************************
 ;; section containing non initialized data
 ;; **********************************************************************
-
 section .bss
 	in_file resb MAX_FILE_SZ
 	file_to_parse resb MAX_FILE_SZ
-	out_file resb MAX_FILE_SZ
 
 ;; **********************************************************************
 ;; section containing code
 ;; **********************************************************************
-
 section .text
 	global _start
 _start:
@@ -58,179 +45,59 @@ _start:
 		read in_file, MAX_FILE_SZ
 		copy_buffer in_file, file_to_parse
 		to_lower file_to_parse
-	run_test1:
+	test1:
 		call individual_tag_test
-	finish_analyzing:
+	end_test:
 		exit
 
 ;; **********************************************************************
 ;; Procedures
-;;
-;; Registers:
-;;			r8: aux_buffer_index, rsp backups
-;;			r9: line_error_location
-;;			r10: column_error_location
-;;			r11: main_buffer_index
-;;			r12: flag check_tag_candidate
 ;; **********************************************************************
-
-;;
-;; get_curr_line: get the current line where r11 buffer index is
-;;				  r9 will contain the result
-;;
-
-get_curr_line:
-	;; auxiliar buffer index
-	mov r8, -1
-	;; contain the number of new lines
-	mov r9, -1
-	.loopa:
-		;; increment and compare
-		inc r8
-		;; test aux buffer index (r8) against buffer index (r9)
-		cmp r8, r11
-		if e
-			;; end count
-			ret
-		endif
-	.count_new_lines:
-		;; test the current byte on buffer against '\n'
-		cmp byte [file_to_parse+r8], 10
-		if e
-			;; store lines quant
-			inc r9
-		endif
-		jmp .loopa
-
-;;
-;; get_curr_col: get the current column where r11 buffer index is
-;;               r10 will have the result
-;;
-
-get_curr_col:
-	;; auxiliar buffer index
-	mov r8, r11
-	;; contain the number of columns
-	mov r10, -1
-	.loopb:
-		;; decrement and compare
-		dec r8
-		;; test aux buffer index (r8) against new line (\n)
-		cmp r8, 10
-		if e
-			;; end count
-			ret
-		else
-			;; store columns quant
-			inc r10
-		endif
-		jmp .loopb
-
-;;
-;; write_integer: write on display the ascii representation of integer
-;; params: rax is register with a number to be writed
-;;
-
-write_integer:
-	;; save stack pointer
-	mov r8, rsp
-    ;; number counter
-    mov rcx, 0
-    itoa:
-        ;; reminder from division
-        mov	rdx, 0
-        ;; base
-        mov	rbx, 10
-        ;; rax = rax / 10
-        div	rbx
-        ;; add \0
-        add	rdx, 48
-        add	rdx, 0x0
-        ;; push reminder to stack
-        push rdx
-        ;; go next
-        inc	rcx
-        ;; check factor with 0
-        cmp	rax, 0
-        ;; loop again
-        jne	itoa
-    write_result:
-        ;; calculate number length
-        lea rdx, [rcx * 8]
-        ;; write number in ascii representation
-        write rsp, rdx
-    clean_stack:
-		mov rsp, r8
-		ret
-
 ;;
 ;; individual_tag_test: check individual tags candidates in xml file
 ;;
-
 individual_tag_test:
-	;; initial message
-	write test1_init, test1_init.len
-	;; start buffer index
-	mov r11, -1
-	;; start flag to check_tag_candidate
-	mov r12, 0
+	;; buffer index
+	mov r8, -1
+	;; boolean check_tag_candidate
+	mov r9, 0
 	.loop:
 		;; increment and compare
-		inc r11
-		cmp r11, MAX_FILE_SZ
+		inc r8
+		cmp r8, MAX_FILE_SZ
 		if e
 			;; end test
 			ret
 		endif
-		;; go to search_tag or check_tag
-		cmp r12, 0
+		;; goto search_tag or check_tag
+		cmp r9, 0
 		if e
 			jmp .search_tag_candidate
 		else
 			jmp .check_tag_candidate
 		endif
 	.search_tag_candidate:
-		cmp byte [file_to_parse+r11], '<'
+		cmp byte [file_to_parse+r8], '<'
 		if e
-			mov r12, 1
+			mov r9, 1
 		else
-			cmp byte [file_to_parse+r11], '>'
+			cmp byte [file_to_parse+r8], '>'
 			if e
-				;; write error location
 				write error1_test1, error1_test1.len
-				;call get_curr_line
-				;mov rax, r9
-				;call write_integer
-				;write DOTS, 1
-				;call get_curr_col
-				;mov rax, r10
-				;call write_integer
-				write NEW_LINE, 1
 			endif
 		endif
-		;; keep searching...
 		jmp .loop
 	.check_tag_candidate:
-		cmp byte [file_to_parse+r11], '>'
+		cmp byte [file_to_parse+r8], '>'
 		if e
-			;; turn off check_tag_candidate
-			mov r12, 0
+			mov r9, 0
 		else
-			cmp byte [file_to_parse+r11], '<'
+			cmp byte [file_to_parse+r8], '<'
 			if e
-				;; write error location
 				write error2_test1, error2_test1.len
-				;call get_curr_line
-				;mov rax, r9
-				;call write_integer
-				;write DOTS, 1
-				;call get_curr_col
-				;mov rax, r10
-				;call write_integer
-				write NEW_LINE, 1
-				;; turn off check_tag_candidate
-				mov r12, 0
+				mov r9, 0
 			endif
 		endif
-		;; keep searching...
 		jmp .loop
+
+;; **********************************************************************
